7a9aaddd65f799cfe3cb3ed09b7f961ab89e10be
Christian Fischer <cf@fisch3r.de>
2019-04-29 10:50:34 +0200

    addition

--- spec/RomanNumberConversion.spec.ts
+++ spec/RomanNumberConversion.spec.ts
@@ -1,13 +1,17 @@
 import convert from "../src/RomanNumberConversion";
 
-describe('Roman Numbers Conversion', () => {
+describe('Roman Numbers Conversion should...', () => {
 
-    it('should convert numbers into their single Roman Digit Representation.', () => {
+    it('convert numbers into their single Roman Digit Representation.', () => {
         let romanNumber = convert(1);
         expect(romanNumber).toBe("I");
         romanNumber = convert(5);
         expect(romanNumber).toBe("V");
-    })
+    });
 
+    it('represent numbers addition Roman Digit Representation.', () => {
+        let romanNumber = convert(1666);
+        expect(romanNumber).toBe("MDCLXVI");
+    });
 
 });
--- src/RomanNumberConversion.ts
+++ src/RomanNumberConversion.ts
@@ -3,12 +3,23 @@ class RomanDigit {
     }
 }
 
-const romanDigits = [new RomanDigit(5, "V"),
+const romanDigits = [
+                new RomanDigit(1000, "M"),
+                new RomanDigit(500, "D"),
+                new RomanDigit(100, "C"),
+                new RomanDigit(50, "L"),
+                new RomanDigit(10, "X"),
+                new RomanDigit(5, "V"),
                 new RomanDigit(1,"I")];
 
-export default function convert(number: number) :String {
+export default function convert(number: number) :string {
+    let romanNumber :string = "";
     for (let digit of romanDigits) {
-        if (number === digit.value)
-            return digit.symbol;
+        if (number/digit.value >= 1) {
+            romanNumber += digit.symbol;
+            number -= digit.value;
+        }
     }
+
+    return romanNumber;
 }
