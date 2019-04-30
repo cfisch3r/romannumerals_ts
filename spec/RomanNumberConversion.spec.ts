6585c1d9152392ac548d3f46a4df4fc7f3d5c9ef
Christian Fischer <cf@fisch3r.de>
2019-04-30 11:25:37 +0200

    mifrate from "it" to "test" as test methods

--- spec/RomanNumberConversion.spec.ts
+++ spec/RomanNumberConversion.spec.ts
@@ -2,31 +2,31 @@ import convert from "../src/RomanNumeralConversion";
 
 describe('Roman Numeral Conversion should process Numbers consisting of', () => {
 
-    it('a single Roman Numeral.', () => {
+    test('a single Roman Numeral.', () => {
         let romanNumber = convert(1);
         expect(romanNumber).toBe("I");
         romanNumber = convert(5);
         expect(romanNumber).toBe("V");
     });
 
-    it('an additive Combination of Roman Numerals.', () => {
+    test('an additive Combination of Roman Numerals.', () => {
         let romanNumber = convert(1666);
         expect(romanNumber).toBe("MDCLXVI");
     });
 
-    it('multiple identical Roman Numerals.', () => {
+    test('multiple identical Roman Numerals.', () => {
         let romanNumber = convert(3);
         expect(romanNumber).toBe("III");
     });
 
-    it('a substractive Combination of Roman Numerals.', () => {
+    test('a substractive Combination of Roman Numerals.', () => {
         let romanNumber = convert(999);
             expect(romanNumber).toBe("CMXCIX");
         romanNumber = convert(444);
         expect(romanNumber).toBe("CDXLIV");
     });
 
-    it('a complex Combination of different Roman Numerals.', () => {
+    test('a complex Combination of different Roman Numerals.', () => {
         let romanNumber = convert(1984);
         expect(romanNumber).toBe("MCMLXXXIV");
     });
